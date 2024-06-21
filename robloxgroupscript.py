d88d4c99b72dbdeac50633b7bc551bc4f911ca71
Smaylis <153941998+Smaylis@users.noreply.github.com>
2024-06-21 17:04:23 +0300

    Update robloxgroupscript.py
    its working now

--- robloxgroupscript.py
+++ robloxgroupscript.py
@@ -2,24 +2,41 @@ import requests
 import time
 groupId = 5574524
 cursor = ''
-limit = 100
-sortOrder = 'Desc'
-count = 0
-file = open(r"SPECIFY THE FILE NAME HERE", "w")
+limit = 10
+sortOrder = 'Asc'
+counter = 0
+file = open(r'C:\Users\Covid_transmitter\Desktop\people.txt', 'w')
+#file = open(r"SPECIFY THE FILE NAME HERE", "w")
 user_req = requests.get(f"https://groups.roblox.com/v1/groups/{groupId}/users?limit={limit}&sortOrder={sortOrder}")
 user_data = user_req.json()
 print(user_data)
-def users():
-    for i in range(100):
-        file.write(user_data["data"][i]["user"]["username"] + '\n')
-        print(user_data["data"][i]["user"]["username"] + '\n')
-try:
-    while user_data["nextPageCursor"] != None:
-        users()
+print(user_data["nextPageCursor"])
+cursor = user_data["nextPageCursor"]
+file.write("hallo")
 
-        user_req = requests.get(
-            f"https://groups.roblox.com/v1/groups/{groupId}/users?limit={limit}&sortOrder={sortOrder}")
+ask = 'y'
+for i in range(limit):
+    #print(user_data['data'][i]['user']['username'])
+    file.write(user_data['data'][i]['user']['username'] + '\n')
+    print(user_data['data'][i]['user']['username'])
+while True:
+    try:
+        user_req = requests.get(f'https://groups.roblox.com/v1/groups/{groupId}/users?limit={limit}&cursor={cursor}&sortOrder={sortOrder}')
+        user_data = user_req.json()
         cursor = user_data["nextPageCursor"]
-except:
-    print("An error has occured")
-    time.sleep(5)
+        
+        for i in range(limit):
+            #print(user_data['data'][i]['user']['username'])
+            file.write(user_data['data'][i]['user']['username'] + '\n')
+            print(user_data['data'][i]['user']['username'])
+
+        counter += limit
+        print(counter)
+        
+    except:
+        print(user_data)
+        ask = str(input("A bug occured, proceed? y/n"))
+        if ask != 'y':
+            exit()
+file.close()
+exit()
